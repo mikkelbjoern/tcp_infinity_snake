7a213c40934036bcfe9cbbb911bb13bab2df8890
Mikkel Goldschmidt <m@goldsch.dk>
2023-02-01 07:24:04 +0100

    Snake working

---
+++ Game.cs
@@ -0,0 +1,236 @@
+// This code represents the game.
+// It exposes the following methods:
+// - An `Action` Enum that represents the possible actions that can be taken
+// - View() which shows how the game should be rendered
+// - Update(`Action`) which updates the game state
+// Internally the game implements a Step() method which
+// is called to make the game progress one step.
+// Control of the game in this manner makes it possible 
+// to correct for network latency and other issues in the future in 
+// the code around the Game class.
+
+using System.Text;
+
+public class Game
+{
+    // Represents the possible actions that can be taken
+    public enum Action { Up, Down, Left, Right, None };
+
+    private enum Direction { North, East, South, West };
+
+    private enum Field { Snake, SnakeHead, Apple, Empty };
+
+    // The game state
+    private Field[,] state;
+
+    // The player's position
+    private int playerX;
+    private int playerY;
+
+    private int score = 0;
+
+    private Direction direction = Direction.East;
+
+    // Whether or not the game is over
+    public bool gameOver = false;
+
+
+    // Represents the tail of the snake (not including the head)
+    private List<Tuple<int, int>> tail = new List<Tuple<int, int>>();
+
+    // The width and height of the game state
+    private int xWidth = 20;
+    private int yHeight = 10;
+
+    // The constructor
+    public Game()
+    {
+        // Create a new game state
+        state = new Field[xWidth, yHeight];
+        // Set the player's position to the center of the game state
+        playerX = xWidth / 2;
+        playerY = yHeight / 2;
+
+        // Set all positions in the game state to empty
+        for (int x = 0; x < xWidth; x++)
+        {
+            for (int y = 0; y < yHeight; y++)
+            {
+                state[x, y] = Field.Empty;
+            }
+        }
+
+        // Set the player's position in the game state to 1
+        state[playerX, playerY] = Field.SnakeHead;
+
+        // Generate an apple
+        generateApple();
+    }
+
+    // Generates an apple in a random position
+    private void generateApple()
+    {
+        // Generate a random position
+        int x = new Random().Next(0, xWidth);
+        int y = new Random().Next(0, yHeight);
+
+        // If the position is not empty, generate a new position
+        while (state[x, y] != Field.Empty)
+        {
+            x = new Random().Next(0, xWidth);
+            y = new Random().Next(0, yHeight);
+        }
+
+        // Set the position to an apple
+        state[x, y] = Field.Apple;
+    }
+
+    // Updates the game state based on user input
+    public void Update(Action action)
+    {
+        // Update the direction based on the action
+        switch (action)
+        {
+            case Action.Up:
+                direction = Direction.North;
+                break;
+            case Action.Down:
+                direction = Direction.South;
+                break;
+            case Action.Left:
+                direction = Direction.West;
+                break;
+            case Action.Right:
+                direction = Direction.East;
+                break;
+        }
+    }
+
+    // Returns the game state as a string
+    public string View()
+    {
+        // If the game is over, write Game over in ascii art
+        if (gameOver)
+        {
+            return "Game Over!";
+        }
+
+        // Create a new string builder
+        StringBuilder sb = new StringBuilder();
+
+        // Loop through the game state
+        for (int y = 0; y < yHeight; y++)
+        {
+            for (int x = 0; x < xWidth; x++)
+            {
+                // If the position is empty, add a space
+                if (state[x, y] == Field.Empty)
+                {
+                    sb.Append(" ");
+                }
+                // If the position is the player, add an X
+                else if (state[x, y] == Field.SnakeHead)
+                {
+                    sb.Append("X");
+                }
+                // If the position is the player, add an X
+                else if (state[x, y] == Field.Snake)
+                {
+                    sb.Append("x");
+                }
+                // If the position is an apple, add an O
+                else if (state[x, y] == Field.Apple)
+                {
+                    sb.Append("O");
+                }
+            }
+            // Add a new line
+            sb.Append(Environment.NewLine);
+        }
+
+        // Return the string
+        return sb.ToString();
+    }
+
+    // Steps the game forward one step
+    public void Step()
+    {
+        // Move the player
+        switch (direction)
+        {
+            case Direction.North:
+                playerY--;
+                break;
+            case Direction.South:
+                playerY++;
+                break;
+            case Direction.West:
+                playerX--;
+                break;
+            case Direction.East:
+                playerX++;
+                break;
+        }
+
+        // If the player is out of bounds, she is dead
+        if (playerX < 0 || playerX >= xWidth || playerY < 0 || playerY >= yHeight)
+        {
+            GameOver();
+            return;
+        }
+
+        // If the player is on an apple, generate a new apple
+        if (state[playerX, playerY] == Field.Apple)
+        {
+            generateApple();
+            score++;
+        }
+        // If the player is on the snake, she is dead
+        else if (state[playerX, playerY] == Field.Snake)
+        {
+            GameOver();
+            return;
+        }
+
+        // Add the player's position to the tail
+        tail.Add(new Tuple<int, int>(playerX, playerY));
+
+
+        // Clear the game state
+        for (int y = 0; y < yHeight; y++)
+        {
+            for (int x = 0; x < xWidth; x++)
+            {
+                // Don't clear the apple
+                if (state[x, y] == Field.Apple)
+                {
+                    continue;
+                }
+                state[x, y] = Field.Empty;
+            }
+        }
+
+        // Set the player's position in the game state
+        state[playerX, playerY] = Field.SnakeHead;
+
+        // If the tail is longer than the score, remove the first element
+        if (tail.Count > score)
+        {
+            tail.RemoveAt(0);
+        }
+
+        // Set the tail's positions in the game state
+        foreach (Tuple<int, int> pos in tail)
+        {
+            state[pos.Item1, pos.Item2] = Field.Snake;
+        }
+
+    }
+
+    // Called when the game is over
+    private void GameOver()
+    {
+        gameOver = true;
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -11,10 +11,112 @@ if (args[0] == "leader")
     // create a leader
     Leader leader = new Leader("127.0.0.1", port);
     // Send 10 commands to the follower in 1 sec intervals
-    for (int i = 0; i < 10; i++)
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     leader.sendCommand($"Hello world! {i}");
+    //     Thread.Sleep(1000);
+    // }
+
+    // Create a new game
+    Game game = new Game();
+
+    // Show the game state
+    Console.WriteLine(game.View());
+
+    // Have a thread that listens for keystrokes
+    Thread thread = new Thread(() =>
     {
-        leader.sendCommand($"Hello world! {i}");
+        // Listen for keystrokes
+        while (true)
+        {
+            // Get the key that was pressed
+            ConsoleKeyInfo key = Console.ReadKey(true);
+
+            // Up arrow
+            if (key.Key == ConsoleKey.UpArrow)
+            {
+                // Update the game state
+                game.Update(Game.Action.Up);
+            }
+            // Down arrow
+            else if (key.Key == ConsoleKey.DownArrow)
+            {
+                // Update the game state
+                game.Update(Game.Action.Down);
+            }
+            // Left arrow
+            else if (key.Key == ConsoleKey.LeftArrow)
+            {
+                // Update the game state
+                game.Update(Game.Action.Left);
+            }
+            // Right arrow
+            else if (key.Key == ConsoleKey.RightArrow)
+            {
+                // Update the game state
+                game.Update(Game.Action.Right);
+            }
+        }
+    });
+
+    thread.Start();
+    // Show 5 steps of the game
+    for (int i = 0; i < 50; i++)
+    {
+        
+        string view = game.View();
+        // Draw the game with a border,
+        // orange for the snake head and body,
+        // red for the apple and
+        // gray for the empty space
+
+        Console.Clear();
+
+        string[] lines = view.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+        Console.WriteLine("┌" + new string('─', lines[0].Length) + "┐");
+        for (int j = 0; j < lines.Length; j++)
+        {
+            // Skip the printing if line is empty
+            if (lines[j].Length == 0)
+                continue;
+
+            Console.Write("│" );
+            for (int k = 0; k < lines[j].Length ; k++)
+            {
+
+                if (lines[j][k] == 'X')
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.Write("X");
+                    Console.ResetColor();
+                }
+                else if (lines[j][k] == 'x')
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.Write("x");
+                    Console.ResetColor();
+                }
+                else if (lines[j][k] == 'O')
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.Write("O");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.Write(" ");
+                    Console.ResetColor();
+                }
+            }
+            Console.WriteLine("│");
+        }
+        Console.WriteLine("└" + new string('─', lines[0].Length) + "┘");
+
+        // Wait for a second
         Thread.Sleep(1000);
+
+        game.Step();
+
     }
 }
 
@@ -24,7 +126,8 @@ else if (args[0] == "follower")
     // create a follower
     Follower follower = new Follower("127.0.0.1", port);
     // Wait for the follower to receive a command
-    while (true) {
+    while (true)
+    {
         follower.waitForCommand();
     }
 }
